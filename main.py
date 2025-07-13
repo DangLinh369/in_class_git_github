ce07c802803b53e2195fe6e456f07b70301fa9a7
GYH <dangmylinh2023pq@gmail.com>
2025-07-13 08:14:06 +0700

    Xoá đoạn bài tập 1 tuần 2 khỏi main.py

--- main.py
+++ main.py
@@ -11,13 +11,20 @@ def Numbers(numb):
 Numbers(datas)
 print(Numbers)
 
-#Bai tap 1 tuan 2
+
+
+#Bài 1 tuần 2 hoàn chỉnh
+num_k_list = []
+result_max_list = []
+
+def get_max_list(num_list, k):
+    for i in range(len(num_list) - k + 1):
+          num_k_list.append(num_list[i:i+k])
+          print("Đoạn", i+1, num_k_list)
+          result_max_list.append(max(num_k_list[-1]))
+          del num_k_list[0]       # Xóa đoạn đầu tiên
+
 num_list = [3, 4, 5, 1, -44, 5, 10, 12, 33, 1]
 k = 3
-result__k = []
-def move_k(k):
-    for item in range(len(num_list)):
-        print(result_k = result_k.append[item:3:1])
-        print("gia tri lon nhat trong k la: " max(result_k))
-    return
-move_k(num_list)
+get_max_list(num_list, k)
+print('Ket qua cua day so lon nhat trong num list voi k = 3 la:', result_max_list)
