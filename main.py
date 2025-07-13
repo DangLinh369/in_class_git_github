37593051e7100cd5d17903a28e86e2ef8f1c2305
GYH <dangmylinh2023pq@gmail.com>
2025-07-13 08:03:08 +0700

    Thêm bài tập 1 tuần 2

--- main.py
+++ main.py
@@ -11,3 +11,13 @@ def Numbers(numb):
 Numbers(datas)
 print(Numbers)
 
+#Bai tap 1 tuan 2
+num_list = [3, 4, 5, 1, -44, 5, 10, 12, 33, 1]
+k = 3
+result__k = []
+def move_k(k):
+    for item in range(len(num_list)):
+        print(result_k = result_k.append[item:3:1])
+        print("gia tri lon nhat trong k la: " max(result_k))
+    return
+move_k(num_list)
